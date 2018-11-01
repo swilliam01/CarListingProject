b360c757b5dcbeaf0d802c83a9884b1d8b51f921
swilliam01 <sehrishwilliam@gmail.com>
2018-10-31 22:35:22 -0400

    i have it

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +12,19 @@ public class HomeController {
     @Autowired
     CarsRepository carsRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
     @RequestMapping("/")
+    public String carsList(Model model){
+        model.addAttribute("carstore", carsRepository.findAll());
+        return "list";
+    }
+    @GetMapping("/add")
+
+    public String carForm(Model model){
+        model.addAttribute("carstores", new Cars());
+        return "carform";
+    }
 
 }
